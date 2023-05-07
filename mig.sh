3750badae7dfcd232dad1cc7559d71b76537159d
Abe Diaz <aberoddia@gmail.com>
2023-05-07 14:47:09 -0400

    Version 0.8 Beta, functional

--- mig.sh
+++ mig.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 #4/25/2023
-#Abrahan Diaz
-#Version 0.1 Alpha
+#Abe Diaz
+#Version 0.8
 #Wherever you are is where I want to be
 
 #TODO
-#3. Detect MegaRAID and VM for hdd and sdd
-#4. PHP modules and versions installed
-#5. Logging
-#6. Detect if DB is even active before reading tables
-#7. Need gather information about the database diretory
+#1. How to check for leftover partitions
 #8. Mysql governor
 #9. cagefs which accounts
 #10. php selector
- 
-#Preliminary
+#cl logic check /var/cagefs/*/*/etc/cl.selector | /var/cagefs/*/*/etc/cl.php.d
 
-#Check if sudo
-if [ "$EUID" -ne 0 ];then 
-	echo "${RED}Not running as root!${NC}"
-	exit 1
-fi
-
+#Global variables
 RED='\033[0;31m'
 GRE='\033[0;32m'
 BLU='\033[0;34m'
@@ -29,62 +19,84 @@ YLW='\033[0;33m'
 NC='\033[0m'
 Date=$(date '+%m-%d-%y')
 
+#Preliminary
+#Check if sudo
+if [ "$EUID" -ne 0 ];then 
+	echo "${RED}Not root!${NC}" 1>&2
+	exit 1
+fi
+
 #Check if Cpanel is installed
 if /usr/local/cpanel/cpanel -V &>/dev/null;then
 	:
 else
-	echo -e "${RED}Cpanel is not installed!${NC}"
+	echo -e "${RED}Cpanel not installed!${NC}" 1>&2
 	exit 1
 fi
 
-#Create directory for all relevant data
-if [[ -d "~/migration-$Date" ]];then
-	cd ~/migration-$Date
-else
-	mkdir ~/migration-$Date
-	cd ~/migration-$Date
-fi
-
-touch accounts.info
-touch database.info
-touch versions.info
-touch drive.info
-
-#Check OS
-echo -e "${BLU}======>OPERATING SYSTEM<======${NC}"
-if grep -q "ubuntu" $(find /etc -iname "os*release");then 
-	os=ubuntu
-	echo -e "$(find /etc -iname "os*release" -exec grep -i "pretty.name" {} \; | awk -F= '{print $2}' | tr -d '"')"
-elif find /etc -iname "redhat*release" &>/dev/null;then
-	if egrep -qi "almalinux" /etc/redhat*release;then
-		os="almalinux"
-		echo -e "$(cat /etc/redhat*release)"
-	elif egrep -qi "centos" /etc/redhat*release;then
-		os="centos"
-		echo -e "$(cat /etc/redhat*release)"
-	elif egrep -qi "cloudlinux" /etc/redhat*release;then
-		os="cloudlinux"
-		echo -e "$(cat /etc/redhat*release)"
-	elif egrep -qi "rockylinux" /etc/redhat*release;then
-		os="rockylinux"
-		echo -e "$(cat /etc/redhat*release)"
-	elif egrep -qi "red.{1,3}hat" /etc/redhat*release;then
-		os="redhat"
-		echo -e "$(cat /etc/redhat*release)"
+basic(){
+	#Create directory for all relevant data
+	if [[ -d "~/migration-$Date" ]];then
+		cd ~/migration-$Date
+	else
+		mkdir ~/migration-$Date
+		cd ~/migration-$Date
 	fi
-else
-	echo "Unsupported OS!"
-	exit 1
-fi
 
-#Install tools
-if [[ $os = "ubuntu" ]];then
-	apt update -y &>/dev/null && apt install smartmontools dmidecode lshw util-linux &>/dev/null
-elif [ $os = "almalinux" ] || [ $os = "centos" ] || [ $os = "rockylinux" ] || [ $os = "cloudlinux" ];then
-	yum install smartmontools dmidecode lswh util-linux -y &>/dev/null
-else
-	dnf install smartmontools dmidecode lshw util-linux -y &>/dev/null
-fi
+	#Check OS
+	echo -e "${BLU}======>OPERATING SYSTEM<======${NC}"
+	if grep -q "ubuntu" $(find /etc -iname "os*release");then 
+		os=ubuntu
+		echo -e "$(find /etc -iname "os*release" -exec grep -i "pretty.name" {} \; | awk -F= '{print $2}' | tr -d '"')"
+	elif find /etc -iname "redhat*release" &>/dev/null;then
+		if egrep -qi "almalinux" /etc/redhat*release;then
+			os="almalinux"
+			echo -e "$(cat /etc/redhat*release)"
+		elif egrep -qi "centos" /etc/redhat*release;then
+			os="centos"
+			echo -e "$(cat /etc/redhat*release)"
+		elif egrep -qi "cloudlinux" /etc/redhat*release;then
+			os="cloudlinux"
+			echo -e "$(cat /etc/redhat*release)"
+		elif egrep -qi "rockylinux" /etc/redhat*release;then
+			os="rockylinux"
+			echo -e "$(cat /etc/redhat*release)"
+		elif egrep -qi "red.{1,3}hat" /etc/redhat*release;then
+			os="redhat"
+			echo -e "$(cat /etc/redhat*release)"
+		fi
+	else
+		echo "Unsupported OS!"
+		exit 1 1>&2
+	fi
+
+	#VM check
+	isVirt=false
+	if dmidecode -s system-manufacturer | egrep -qi "innotek|GmbH|QEMU";then
+		isVirt=true
+	fi
+	if dmidecode -s system-product-name | egrep -qi "Virtual|Standard PC";then
+		isVirt=true
+	fi
+	if lscpu | egrep -qi "Hypervisor vendor: [a-z]+";then
+		isVirt=true
+	fi
+	if lshw -class system | egrep -qi "innotek|GmbH|QEMU";then
+		isVirt=true
+	fi
+	if $isVirt;then
+		echo -e "${RED}Virtual Machine has been detected!${NC}"
+	fi
+	
+	#Install tools
+	if [[ $os = "ubuntu" ]];then
+		apt update -y &>/dev/null && apt install smartmontools dmidecode lshw util-linux &>/dev/null
+	elif [ $os = "almalinux" ] || [ $os = "centos" ] || [ $os = "rockylinux" ] || [ $os = "cloudlinux" ];then
+		yum install smartmontools dmidecode lswh util-linux -y &>/dev/null
+	else
+		dnf install smartmontools dmidecode lshw util-linux -y &>/dev/null
+	fi
+}
 
 #Drive Information
 drives(){
@@ -148,16 +160,15 @@ versions(){
 accounts(){
 	echo -e "${BLU}======>DOMAINS<======${NC}"
 	for acc in $(/usr/local/cpanel/bin/whmapi1 --output=jsonpretty   listaccts 2>/dev/null | grep user | awk '{print $3}' | sed 's/[",]//g');do
-        
-		adom=$(/usr/local/cpanel/bin/whmapi1 accountsummary user=$acc 2>/dev/null | grep "domain:" | awk '{print $2}' )
+		adom=$(/usr/local/cpanel/bin/whmapi1 accountsummary user=$acc 2>/dev/null | grep "domain:" | awk '{print $2}')
         aip=$(/usr/local/cpanel/bin/whmapi1 accountsummary user=$acc 2>/dev/null | grep "ip:" | awk '{print $2}')
         asus=$(/usr/local/cpanel/bin/whmapi1 accountsummary user=$acc 2>/dev/null | grep " suspended:" | awk '{print $2}')
 		ahome=$(grep "documentroot:" /var/cpanel/userdata/$acc/$adom | awk '{print $2}')
 		assl=$(grep "ssl_redirect:" /var/cpanel/userdata/$acc/$adom | awk '{print $2}')
 		aphp=$(grep "phpversion:" /var/cpanel/userdata/$acc/$adom | awk '{print $2}')
-
-        echo -e "${YLW}++++++$adom++++++${NC}"
+        echo -e "${GRE}++++++$acc++++++${NC}"
 		#Load
+		echo -e "${YLW}====>Main: $adom${NC}"
         if curl -sIL $adom | egrep -q "HTTP.{3,5} 200 OK";then
             echo -e "${GRE}$adom loads 200${NC}"
         else
@@ -181,48 +192,127 @@ accounts(){
         echo -e "Domain Cpanel IP: $aip"
         echo -e "A record: $(dig @8.8.8.8 A $adom +short | paste -sd" ")"
         echo -e "NS record: $(dig @8.8.8.8 NS $adom +short | paste -sd" ")"
-
-	done
+		#Account subdomains
+		for $asub in $(grep grep "^  - " /var/cpanel/userdata/$acc/main | awk '{print $2}');do
+			shome=$(grep "documentroot:" /var/cpanel/userdata/$acc/$asub | awk '{print $2}')
+			sssl=$(grep "ssl_redirect:" /var/cpanel/userdata/$acc/$asub | awk '{print $2}')
+			sphp=$(grep "phpversion:" /var/cpanel/userdata/$acc/$asub | awk '{print $2}')
+			sip=$(grep "ip:" /var/cpanel/userdata/$acc/$adom | awk '{print $2}')
+			#Load
+			echo -e "${YLW}====>Sub: $asub${NC}"
+			if curl -sIL $asub | egrep -q "HTTP.{3,5} 200 OK";then
+				echo -e "${GRE}$asub loads 200${NC}"
+			else
+				echo -e "${RED}$asub did not load${NC}"
+			fi
+			#SSL
+			if [[ $sssl -eq "1" ]];then
+				echo -e "${GRE}SSL redirect enabled${NC}"
+				scurlout=$(curl -svL $asub 2>&1)
+				if echo $scurlout | egrep -q "SSL certificate verify ok";then
+					echo -e "${GRE}$asub SSL is good, expires: $(echo $scurlout | egrep -o "expire date:.+20[0-9]{2} [A-Z]{2,4}" | sed 's/expire date://' )${NC}"
+				else
+					echo -e "${RED}$asub SSL not working${NC}"
+				fi
+			else
+				echo -e "${RED}SSL redirect disabled${NC}"
+			fi
+			#PHP
+			echo -e "PHP version: $sphp"
+			#DNS
+			echo -e "Domain Cpanel IP: $aip"
+			echo -e "A record: $(dig @8.8.8.8 A $asub +short | paste -sd" ")"
+			echo -e "NS record: $(dig @8.8.8.8 NS $asub +short | paste -sd" ")"
+		done
+	done  
 }
 
 #DATABASES
 database(){
 	echo -e "${BLU}======>DATABASES<======${NC}"
+	if mysql -e "SHOW DATABASES;" &>/dev/null;then
+		:
+	else
+		echo "${RED}MySQL/MariaDB not active or not accsseible via root without password. Skipping database checking!${NC}" 1>&2
+		return
+	fi
+	if grep -q "datadir=" /etc/my.cnf;then
+		ddir=$(grep "datadir=" /etc/my.cnf | awk -F= '{print $2}')
+	else
+		ddir="/var/lib/mysql"
+	fi
+	echo -e "Databse location: $ddir"
+	dsize=$(du -s $ddir | awk '{print $1}')
+	if [[ $dsize -gt 20000000 ]];then
+		echo -e "Database Size:${RED} $(echo $dsize | awk '$1/1024 > 1000{printf "%.2fGB\n", $1/(1000*1024)}$1/1024 < 1000{printf "%.2fMB\n", $1/1024}')"
+	else
+		echo -e "Database Size:${GRE} $(echo $dsize | awk '$1/1024 > 1000{printf "%.2fGB\n", $1/(1000*1024)}$1/1024 < 1000{printf "%.2fMB\n", $1/1024}')"
+	fi
 	for db in $(mysql -e "SHOW DATABASES;"| awk 'NR != 1 && $1 != "information_schema" && $1 != "performance_schema" && $1 != "mysql" && $1 != "sys"{print $1}');do
 		echo -e "${YLW}++++++$db++++++${NC}"
 		for tb in $(mysql -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA LIKE \"$db\";" | awk 'NR != 1 {print $1}');do
 			if mysql -e "SELECT * FROM $db.$tb LIMIT 10;" &>/dev/null;then
-				echo -e "Database: $db Table: $tb reads"
+				echo -e "Database: $db Table: $tb ${GRE}reads${NC}"
 			else
-				echo -e "Database: $db Table: $tb did not read"
+				echo -e "Database: $db Table: $tb  ${RED}not read${NC}"
 			fi
 		done
+	done 
+}
+
+#Pull various configuration
+config(){
+	/usr/local/cpanel/bin/cpconftool --modules=cpanel::smtp::exim,cpanel::system::backups,cpanel::system::whmconf,cpanel::easy::apache, --backup
+	cp /etc/my.cnf ./my.cnf.back
+	echo -e "${BLU}======>PHP<======${NC}"
+	for php in $(ls /opt/cpanel | grep php);do
+		echo -e "++++++$php Modules++++++" >> php.modules
+		/opt/cpanel/$php/root/usr/bin/php -m | egrep "^[a-zA-Z]" >> php.modules
 	done
 }
 
-#Virtual Machine
-#Detect VM OS
-virtual(){
-	isVirt=false
-	if dmidecode -s system-manufacturer | egrep -qi "innotek|GmbH|QEMU";then
-		isVirt=true
-	fi
-	if dmidecode -s system-product-name | egrep -qi "Virtual|Standard PC";then
-		isVirt=true
-	fi
-	if lscpu | egrep -qi "Hypervisor vendor: [a-z]+";then
-		isVirt=true
-	fi
-	if lshw -class system | egrep -qi "innotek|GmbH|QEMU";then
-		isVirt=true
-	fi
-	if $isVirt;then
-		echo -e "${RED}Virtual Machine has been detected!${NC}"
-	fi
-}
+if [[ $1 == "" ]];then
+	basic
+	cd ~/migration-$Date
+	config
+	drives | tee -a drive.info
+	versions | tee -a versions.info
+	accounts | tee -a accounts.info
+	database | tee -a database.info
+	exit
+fi
 
-virtual
-drives
-versions
-accounts
-database
+while getopts "asdh" opt; do
+	case $opt in
+		a)
+			basic
+			cd ~/migration-$Date
+			config
+			drives | tee -a drive.info
+			versions | tee -a versions.info
+			accounts | tee -a accounts.info
+			database | tee -a database.info
+			;;
+		s)
+			accounts | tee -a accounts.info
+			;;
+		d)
+			database | tee -a accounts.info
+			;;
+		h)
+			cat << EOF
+	-a All checks, same as no argument.
+
+	-s Check only domains.
+
+	-d Check only databases.
+
+	-h Display help 
+EOF
+			;;
+		\?)
+			exit 1
+			;;
+	esac
+done
+
