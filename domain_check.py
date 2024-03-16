5d355365156f6ac100e18a94afbfcb24438f3169
Abe Diaz <aberoddia@gmail.com>
2024-03-16 02:54:00 -0400

    Added more of the SSL logic check. Also added status code phrases. TODO: verify exception handling, test various enviorments.

--- domain_check.py
+++ domain_check.py
@@ -1,85 +1,108 @@
 #!/usr/bin/env python3
 
-import certifi
 import re
 import requests
 import socket
 import ssl
 import subprocess
 import urllib3
-from typing import Dict, List
+
+from datetime import datetime, timedelta
+from http import HTTPStatus
+from typing import List
 
 class Domain_checker:
 
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    END = '\033[0m'
+    RED: str = '\033[91m'
+    GREEN: str = '\033[92m'
+    YELLOW: str = '\033[93m'
+    END: str = '\033[0m'
     domain_list: List[str] = []
-    domain_list_respone: Dict[str, List[str]] = {}
     local_ips: List[str] = []
 
     def __init__(self, local_ips: List[str], domain_list: List[str]) -> None:
         self.domain_list = domain_list
         self.local_ips = local_ips
         urllib3.disable_warnings()
+        try:
+            requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+        except:
+            return
 
-    def write(self, message: str) -> None:
-        with open('domain.log', 'a') as file:
+    def log(self, message: str) -> None:
+        with open('domains.log', 'a') as file:
             file.write(message + '\n')
+
+    def write(self, domain: str) -> None:
+        with open('domains.final', 'a') as file:
+            match = re.search(r'(https?://)([^/]+)(/?.*)', domain)
+            if match:
+                 host: str = match.group(2)
+            else:
+                return
+            file.write(host + '\n')
 
     def verify_ssl(self, domain: str) -> None:
         match = re.search(r'(https://)([^/]+)(/?.*)', domain)
         if match:
-            host = match.group(2)
+             host: str = match.group(2)
         else:
             return
-        context = ssl.create_default_context(cafile=certifi.where())
+        context = ssl.create_default_context()
         with socket.create_connection((host, 443)) as sock:
             with context.wrap_socket(sock, server_hostname=host) as secure_sock:
                 secure_sock.do_handshake()
                 cert = secure_sock.getpeercert()
                 if cert:
-                    print(cert.values())
+                    if cert['issuer'] == cert['subject']:
+                        message: str = (domain + self.YELLOW + ' SSL is self signed.' + self.END)
+                        print(message)
+                        self.log(message)
+                    exipration = datetime.strptime(str(cert['notAfter']), '%b %d %H:%M:%S %Y %Z')
+                    if datetime.now() > exipration - timedelta(days=30):
+                        message: str = (domain + self.YELLOW + ' SSL will expire in less than 30 days.' + self.END)
+                        print(message)
+                        self.log(message)
 
     def check(self, domain: str, response: requests.Response) -> None:
-        if response.status_code >= 500:
-            message: str = (domain + self.RED + ' had server side error: ' + str(response.status_code) + self.END)
+        code: int = response.status_code
+        if code >= 500:
+            message: str = (domain + self.RED + ' had server side error: ' + str(code) + ' ' + HTTPStatus(code).phrase + self.END)
             print(message)
-            self.write(message)
-        elif response.status_code < 500 and response.status_code >= 400:
-            message: str = (domain + self.RED + ' had client side error: ' + str(response.status_code) + self.END)
+            self.log(message)
+        elif code < 500 and code >= 400:
+            message: str = (domain + self.RED + ' had client side error: ' + str(code) + ' ' + HTTPStatus(code).phrase + self.END)
             print(message)
-            self.write(message)
+            self.log(message)
         else:
             warning: bool = False
+            self.write(response.url)
             if len(response.history) >= 10:
                 message: str = (domain + self.YELLOW + ' responded but had 10 or more redirects.' + self.END)
                 print(message)
-                self.write(message)
+                self.log(message)
                 warning = True
             if socket.gethostbyname(domain) not in self.local_ips:
                 message: str = (domain + self.YELLOW + ' did not resolve to an internal ip.' + self.END)
                 print(message)
-                self.write(message)
+                self.log(message)
                 warning = True
             if not re.search(domain, response.url):
                 message: str = (domain + self.YELLOW + ' not contained in final redirect.' + self.END)
                 print(message)
-                self.write(message)
+                self.log(message)
                 warning = True
             if 'https' in response.url:
                 try:
                     self.verify_ssl(response.url)
                 except:
-                    message: str = (domain + self.YELLOW + ' SSL verification error.' + self.END)
+                    message: str = (domain + self.RED + ' SSL verification error.' + self.END)
                     print(message)
-                    self.write(message)
+                    self.log(message)
                     warning = True
             if not warning:
                 message: str = (domain + self.GREEN + ' ok' + self.END)
-                print(message)
-                self.write(message)
+                self.log(message)
 
     def run(self) -> None:
         domain: str
@@ -91,21 +114,21 @@ class Domain_checker:
                     ip = socket.gethostbyname(domain)
                     message: str = (domain + self.RED + ' located in ' + ip + ', did not respond.' + self.END)
                     print(message)
-                    self.write(message)
+                    self.log(message)
                 except socket.gaierror:
                     message: str = (domain + self.RED + ' did not resolve.' + self.END)
                     print(message)
-                    self.write(message)
+                    self.log(message)
             except requests.Timeout:
                 message: str = (domain + self.RED + ' did not respond in time.' + self.END)
                 print(message)
-                self.write(message)
+                self.log(message)
             except requests.TooManyRedirects:
                 message: str = (domain + self.RED + ' had too many redirects.' + self.END)
                 print(message)
-                self.write(message)
+                self.log(message)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     domain_list: List[str] = []
     local_ips: List[str] = []
     lines: List[str] = subprocess.check_output(['whmapi1', 'get_domain_info']).decode().split('\n')
